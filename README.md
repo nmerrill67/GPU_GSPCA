08983fec576f018c6137aff828cd05389a8d4092
nmerrill67 <nmerrill@udel.edu>
2017-09-14 10:29:17 -0400

    Transitioning to cublas functions

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Gram-Schmidt-PCA
-Implements PCA using NIPALS algorithm. Also uses GRAM-SCMIDT method to avoid the problem of non-orthagonal PCs by NIPALS.
-Uses Numpy to implement non-GPU version stated in http://arxiv.org/pdf/0811.1081.pdf .
+Implements PCA GRAM-SCMIDT method to avoid the problem of non-orthagonal PCs by NIPALS.
+Uses Pycuda to implement GPU version stated in http://arxiv.org/pdf/0811.1081.pdf .
--- gspca_gpu.py
+++ gspca_gpu.py
@@ -1,8 +1,7 @@
 
 import numpy as np
 from pycuda import gpuarray, autoinit, cumath
-from skcuda import linalg
-
+from skcuda import cublas
 
 def preprocessmat(X):
 	X -= X.mean()
@@ -10,31 +9,30 @@ def preprocessmat(X):
 #Look into http://en.wikipedia.org/wiki/Gram%E2%80%93Schmidt_process for a glimpse into this method
 # Also in http://arxiv.org/pdf/0811.1081.pdf
 # run preprocessmat! before running this
-def CUDA_GSPCA(X,num_pcs,epsilon=0.0001, max_iter=10000):
+def CUDA_GSPCA(X, num_pcs, epsilon=0.0001, max_iter=10000):
 	
-	linalg.init() # intialize the gpu linalg library
+	cublas = cublas.cublasCreate() # intialize the gpu linalg library
 
 	R = gpuarray.to_gpu(X) # move data to gpu
 
-	V = gpuarray.zeros_like(R)
-
 	Lambda = gpuarray.zeros((R.shape[0],1), np.float32)
 
-	U = gpuarray.zeros((X.shape[1],X.shape[1]), np.float32)
+	P = gpuarray.zeros((X.shape[1],X.shape[1]), np.float32)
+
+	T = gpuarray.zeros((X.shape[0],num_pcs), np.float32)
 
 	for k in xrange(num_pcs):
 
 		print k,"th PC loop"
 		mu = 0.0
-		V[:,k] = R[:,k]
+		T[:,k] = R[:,k]
 
 		for j in xrange(max_iter):
 
-			print R.shape, V.shape
+			
+			P[:,k] = linalg.mdot(linalg.transpose(R),T[:,k])
 
-
-			U[:,k] = linalg.mdot(linalg.transpose(R),V[:,k])
-
+						
 			if k > 0:
 				A = linalg.dot(linalg.transpose(U[:,k]), U[:,k]) 
 				U[:,k] -= U[:,k] * A
@@ -59,7 +57,9 @@ def CUDA_GSPCA(X,num_pcs,epsilon=0.0001, max_iter=10000):
 	T = linalg.mdot(V, Lambda)
 	P = U
 
-	return T#,P,R
+	cublasDestroy(cublas)
+
+	return T.get() # return the cpu data
 
 if __name__=='__main__':
 	X = np.random.rand(400,56).astype(np.float32)
