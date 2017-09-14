1e4a75288619e9b7250de389dba7d9f14eafcb15
nmerrill67 <nmerrill@udel.edu>
2017-09-14 15:16:20 -0400

    Closer to working version ...

--- gspca_gpu.py
+++ gspca_gpu.py
@@ -6,7 +6,7 @@ from skcuda import cublas, misc
 #Look into http://en.wikipedia.org/wiki/Gram%E2%80%93Schmidt_process for a glimpse into this method
 # Also in http://arxiv.org/pdf/0811.1081.pdf
 # run preprocessmat! before running this
-def CUDA_GSPCA(X, num_pcs, epsilon=0.0001, max_iter=10000):
+def CUDA_GSPCA(X, num_pcs, epsilon=0.0000001, max_iter=10000):
 	
 	h = cublas.cublasCreate() # create a handle to the c library
 
@@ -26,34 +26,40 @@ def CUDA_GSPCA(X, num_pcs, epsilon=0.0001, max_iter=10000):
 
 
 	# mean centering data
-	U = misc.sum(R,axis=1) # sum the columns of R
+	U = misc.sum(R,axis=0) # sum the columns of R
 
-	print p
-	print U.shape
+
 
 	for i in xrange(p):
-		R[:,i] -= U / p # Subtract the mean from each row. Data is now mean centered 	
+		cublas.cublasSaxpy(h, n, -1.0/p, U.gpudata, 1, R[:,i].gpudata, 1) 	
+
+	Lk = 0.0
 
 	for k in xrange(num_pcs):
 
-		print k,"th PC loop"
+		print k," PC loop"
+
 		mu = 0.0
 
-		T[:,k] = R[:,k]
+		cublas.cublasDcopy(h, n, R[:,k].gpudata, 1, T[:,k].gpudata, 1)
 
 		for j in xrange(max_iter):
 
-			
+		
+			print j, 'inner loop'
+	
 			cublas.cublasSgemv(h, 't', n, p, 1.0, R.gpudata, n, T[:,k].gpudata, 1, 0.0, P[:,k].gpudata, 1)
-
+	
 						
 			if k > 0:
 
-				cublas.cublasSgemv(h,'t', p, k, P.gpudata, p, P[:,k].gpudata, 1, 0.0, U, 1)  
+				cublas.cublasSgemv(h,'t', p, k, 1.0, P.gpudata, p, P[:,k].gpudata, 1, 0.0, U.gpudata, 1)  
 
-				cublas.cublasSgemv (h, 'n', p, k, -1.0, P, p, U, 1, 1.0, P[:,k].gpudata, 1)
+				cublas.cublasSgemv (h, 'n', p, k, -1.0, P.gpudata, p, U.gpudata, 1, 1.0, P[:,k].gpudata, 1)
 
-			cublas.cublasSscal(h, p, 1.0/cublas.cublasSnrm2(h, p, P[:,k].gpudata, 1), P[:,k].gpudata, 1)
+
+			l2 = cublas.cublasSnrm2(h, p, P[:,k].gpudata, 1)
+			cublas.cublasSscal(h, p, 1.0/l2, P[:,k].gpudata, 1)
 
 			cublas.cublasSgemv(h, 'n', n, p, 1.0, R.gpudata, n, P[:,k].gpudata, 1, 0.0, T[:,k].gpudata, 1)
 
@@ -63,24 +69,37 @@ def CUDA_GSPCA(X, num_pcs, epsilon=0.0001, max_iter=10000):
 				cublas.cublasSgemv (h, 'n', n, k, -1.0, T.gpudata, n, U.gpudata, 1, 1.0, T[:,k].gpudata, 1)
 		
 
-			Lambda[k].gpudata = cublas.cublasSnrm2(h, n, T[:,k].gpudata, 1)
-			cublas.cublasSscal(n, 1.0/L[k], T[:,k].gpudata, 1)
+			Lambda[k,:] = np.array([cublas.cublasSnrm2(h, n, T[:,k].gpudata, 1)], dtype=np.float32)
+
+
+			Lk = Lambda[k].get()[0]
+			cublas.cublasSscal(h, n, 1.0/Lk, T[:,k].gpudata, 1)
 		
+			
+			
 
-
-			if cummath.fabs(Lambda[k]-mu) < epsilon:
+			if abs(Lk-mu) < epsilon:
 				break
 
-			mu = Lambda[k]
+			mu = Lk
 
-		cublas.cublasSger (h, n, p, -Lambda[k], T[:,k].gpudata, 1, P[:,k].gpudata, 1, R.gpudata, n)
+		# end for j
+
+		print 'before update R', P.gpudata, '\n'
+		cublas.cublasSger(h, n, p, (0-Lk), T[:,k].gpudata, 1, P[:,k].gpudata, 1, R.gpudata, n)
+
+		print 'after update R', P.gpudata,'\n'
+
+	# end for k
 
 	for k in xrange(num_pcs):
-		cublas.cublasDscal(h, n, Lambda[k], T[:,k].gpudata, 1) 
+		cublas.cublasDscal(h, n, Lk, T[:,k].gpudata, 1) 
 
-	cublasDestroy(h)
+	T_cpu = T.get()
 
-	return T.get() # return the cpu data
+	cublas.cublasDestroy(h)
+
+	return T_cpu # return the cpu data
 
 if __name__=='__main__':
 	X = np.random.rand(400,56).astype(np.float32)
