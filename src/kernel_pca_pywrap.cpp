e6cdedbba764ddb6fb3daa389647a4df1c406a6e
nmerrill67 <nmerrill@udel.edu>
2017-09-20 00:56:00 -0400

    not compiling yet

--- src/kernel_pca_pywrap.cpp
+++ src/kernel_pca_pywrap.cpp
@@ -5,9 +5,9 @@ BOOST_PYTHON_MODULE(kernel_pca)
 {
 
 	boost::python::class_<KernelPCA>("KernelPCA",
-		init<int>())
+		boost::python::init<int>())
 		.def("fit_transform", &KernelPCA::fit_transform)
-		.add_property("K", &KernelPCA::set_n_components)	
+		.def("set_n_components", &KernelPCA::set_n_components)	
 	;
 
 }
