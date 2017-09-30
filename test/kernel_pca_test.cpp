0cf2c4ead1d7cd65a64bada5732bb5a1a79035ef
Patrick Geneva <pgeneva@udel.edu>
2017-09-30 18:06:25 -0400

    tests passing

--- test/kernel_pca_test.cpp
+++ test/kernel_pca_test.cpp
@@ -6,7 +6,6 @@
 
 KernelPCA* test_pca;
 
-int K = 2;
 
 int M = 1000, m;
 int N = 100, n;
@@ -57,21 +56,29 @@ TEST(kernel_pca, fit_transform_all_double_test)
 
 } 
 
+int gsl_status;
+
 TEST(kernel_pca, double_all_orth_test)
 {
 	// check  that the bases are orthagonal
-	gsl_matrix* T_mat = gsl_matrix_alloc(M, K);
+	gsl_matrix* T_mat = gsl_matrix_alloc(M, N);
 
 
 	for (m=0; m<M; m++)
 	{
 		for (n=0; n<N; n++)
 		{
-			gsl_matrix_set(T_mat, m, n, Td[ind_f(m,n,M)]);
+			try
+			{
+				gsl_matrix_set(T_mat, m, n, Td[ind_f(m,n,M)]);
+			}	
+			catch(...) 
+			{
+				FAIL() << "Error getting data from double precision principal component score at (" << m << ", " << n << ")\n"; 
+			}
 		}
 	}
 
-	// std::cout << "T:\n\n" << T_string << "\n\n";
 	
 	double dot_product;
 
@@ -82,8 +89,9 @@ TEST(kernel_pca, double_all_orth_test)
 		const gsl_vector T0 = gsl_matrix_column(T_mat, i-1).vector;
 		const gsl_vector T1 = gsl_matrix_column(T_mat, i).vector;
 
-		gsl_blas_ddot(&T0, &T1, &dot_product);
-
+		gsl_status = gsl_blas_ddot(&T0, &T1, &dot_product);
+		
+		EXPECT_EQ(gsl_status, 0) << "GSL double precision dot product failed";
 
 		EXPECT_LT(dot_product, max_dot_allowed) << "double precision fit_transform for all components created weakly orthogonal eigenvectors";
 
@@ -127,18 +135,26 @@ TEST(kernel_pca, fit_transform_all_float_test)
 TEST(kernel_pca, float_all_orth_test)
 {
 	// check  that the bases are orthagonal
-	gsl_matrix_float* T_mat = gsl_matrix_float_alloc(M, K);
+	gsl_matrix_float* T_mat = gsl_matrix_float_alloc(M, N);
 
 
 	for (m=0; m<M; m++)
 	{
-		for (n=0; n<K; n++)
+		for (n=0; n<N; n++)
 		{
-			gsl_matrix_float_set(T_mat, m, n, Tf[ind_f(m,n,M)]);
+			try
+			{
+				gsl_matrix_float_set(T_mat, m, n, Tf[ind_f(m,n,M)]);
+
+			}
+                        catch(...)
+                        {
+                                FAIL() << "Error getting data from single precision principal component score at (" << m << ", " << n << ")\n";                  
+                        }
+
 		}
 	}
 
-	// std::cout << "T:\n\n" << T_string << "\n\n";
 	
 	float dot_product;
 
@@ -160,20 +176,174 @@ TEST(kernel_pca, float_all_orth_test)
 
 	free(Tf);	
 
+	delete test_pca;
+
 }
-/*
+
+
+KernelPCA* test_pca2;
+
+int K = 2;
+
 TEST(kernel_pca, constructor_test)
 {
-	test_pca = new KernelPCA();
-	EXPECT_TRUE(test_pca); // make sure its not a null pointer
-	EXPECT_EQ(test_pca->get_n_components(), -1);
+	test_pca2 = new KernelPCA(4);
+	EXPECT_TRUE(test_pca2); // make sure its not a null pointer
+	EXPECT_EQ(test_pca2->get_n_components(), 4);
 }
 
 TEST(kernel_pca, set_get_n_comp_test)
 {
 
-	test_pca->set_n_components(K);
+	test_pca2->set_n_components(K);
 
-	EXPECT_EQ(test_pca->get_n_components(), K); 
+	EXPECT_EQ(test_pca2->get_n_components(), K); 
 
-}*/
+}
+
+
+TEST(kernel_pca, fit_transform_double_test)
+{
+
+	// initiallize some random test data X
+	double *Xd;
+
+
+        Xd = (double*)malloc(M*N * sizeof(Xd[0]));
+
+        for(m = 0; m < M; m++)
+        {
+                for(n = 0; n < N; n++)
+                {
+        	        Xd[ind_f(m, n, M)] = rand() / (double)RAND_MAX;
+                }
+        }
+
+	
+
+        Td = test_pca2->fit_transform(M, N, Xd, 1);
+
+	EXPECT_TRUE(Td) << "double-precision fit_transform for 2 components returned a null pointer";
+
+	free(Xd);	
+
+} 
+
+
+TEST(kernel_pca, double_orth_test)
+{
+	// check  that the bases are orthagonal
+	gsl_matrix* T_mat = gsl_matrix_alloc(M,K);
+
+
+	for (m=0; m<M; m++)
+	{
+		for (n=0; n<K; n++)
+		{
+			try
+			{
+				gsl_matrix_set(T_mat, m, n, Td[ind_f(m,n,M)]);
+			}	
+			catch(...) 
+			{
+				FAIL() << "Error getting data from double precision principal component score at (" << m << ", " << n << ")\n"; 
+			}
+		}
+	}
+
+	
+	double dot_product;
+
+	double max_dot_allowed = 0.0000001; // dot product of the orthagonal eigenvectors should be  close to zero. So lets compare it
+
+
+	const gsl_vector T0 = gsl_matrix_column(T_mat, 0).vector;
+	const gsl_vector T1 = gsl_matrix_column(T_mat, 1).vector;
+
+	gsl_status = gsl_blas_ddot(&T0, &T1, &dot_product);
+	
+	EXPECT_EQ(gsl_status, 0) << "GSL double precision dot product failed";
+
+	EXPECT_LT(dot_product, max_dot_allowed) << "double precision fit_transform for two components created weakly orthogonal eigenvectors";
+
+
+	gsl_matrix_free(T_mat);
+
+	free(Td);	
+
+}
+
+
+TEST(kernel_pca, fit_transform_float_test)
+{
+
+	// initiallize some random test data X
+	float *Xf;
+
+
+        Xf = (float*)malloc(M*N * sizeof(Xf[0]));
+
+        for(m = 0; m < M; m++)
+        {
+                for(n = 0; n < N; n++)
+                {
+        	        Xf[ind_f(m, n, M)] = rand() / (float)RAND_MAX;
+                }
+        }
+
+	
+
+        Tf = test_pca2->fit_transform(M, N, Xf, 1);
+
+	EXPECT_TRUE(Tf) << "single precision fit_transform for all components returned a null pointer";
+
+	free(Xf);	
+
+} 
+
+TEST(kernel_pca, float_orth_test)
+{
+	// check  that the bases are orthagonal
+	gsl_matrix_float* T_mat = gsl_matrix_float_alloc(M, K);
+
+
+	for (m=0; m<M; m++)
+	{
+		for (n=0; n<K; n++)
+		{
+			try
+			{
+				gsl_matrix_float_set(T_mat, m, n, Tf[ind_f(m,n,M)]);
+
+			}
+                        catch(...)
+                        {
+                                FAIL() << "Error getting data from single precision principal component score at (" << m << ", " << n << ")\n";                  
+                        }
+
+		}
+	}
+
+	
+	float dot_product;
+
+	float max_dot_allowed = 0.0001;	
+
+
+	const gsl_vector_float T0 = gsl_matrix_float_column(T_mat, 0).vector;
+	const gsl_vector_float T1 = gsl_matrix_float_column(T_mat, 1).vector;
+
+	gsl_blas_sdot(&T0, &T1, &dot_product);
+
+
+	EXPECT_LT(dot_product, max_dot_allowed) << "single precision fit_transform for all components created weakly orthogonal eigenvectors";
+
+	gsl_matrix_float_free(T_mat);
+
+	free(Tf);	
+
+	delete test_pca2;
+
+}
+
+
